bcf472064e3fa9ea20c8ccc90264317b8c5114f5
ShowCaseCode <showcasecode@outlook.com>
2021-04-17 00:31:04 -0400

    11th commit for April 17, 2021

--- morejs.js
+++ morejs.js
@@ -21,8 +21,10 @@ Int16Array;
 force();
 force();
 
+//Testing name of villian
 let name_of_villian = "Darkseid";
 console.log(name_of_villian);
 
+//Testing name of hero
 let name_of_hero = "Superman";
 console.log(name_of_hero);

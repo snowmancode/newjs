d1a392e5fc1d10552aab771d35c444afa40778f0
ShowCaseCode <showcasecode@outlook.com>
2021-04-20 15:22:09 -0400

    1st commit on function declaration on April 20, 2021 in the afternoon

--- r2.js
+++ r2.js
@@ -9,3 +9,17 @@ let person = {
 console.log(person);
 person.name = "Bruce";
 console.log(person);
+
+let selectedColors = ['red', 'white', 'blue'];
+console.log(selectedColors);
+console.log(selectedColors[0, 1]);
+console.log(selectedColors.length);
+console.log(selectedColors.keys);
+console.log(selectedColors.reverse);
+console.log(selectedColors);
+
+function welcome() {
+    console.log("Hello.  I am the Coyote.");
+}
+
+welcome();
